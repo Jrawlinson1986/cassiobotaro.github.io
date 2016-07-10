2706a3b6c7b8e71620d8483ad155455c5ffe5405
cassiobotaro <cassiobotaro@gmail.com>
2016-07-10 02:06:26 -0300

    https now

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://cassiobotaro.github.io'
+SITEURL = 'https://cassiobotaro.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
